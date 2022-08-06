0d4b8619b0829365bdab8f288959c64f3ae77be4
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2022-08-06 13:20:24 +0300

    ga id update

--- constants/common.ts
+++ constants/common.ts
@@ -2,7 +2,7 @@ export const META = {
   title: "Yiğithan Yücedağ",
   description: "Software Developer & UI/UX Designer",
   url: "https://yigithan.dev",
-  ga: "UA-17768654-1",
+  ga: "UA",
   author: {
     email: "yigithanyucedag@gmail.com",
     name: "Yiğithan Yücedağ",
