a89463a667de0b6d3f8785491486b9fcd2782690
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2022-08-06 18:37:22 +0300

    twitter transformer added

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,6 +2,29 @@ import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import readingTime from "reading-time";
 import rehypePrism from "rehype-prism-plus";
 
+// Node currently can’t pick up on the default exports by `@remark-embedder`.
+import fauxRemarkEmbedder from "@remark-embedder/core";
+//@ts-ignore
+const remarkEmbedder = fauxRemarkEmbedder.default;
+
+const TwitterTransformer = {
+  name: "Twitter",
+  // shouldTransform can also be async
+  shouldTransform(url: string) {
+    const { host, pathname } = new URL(url);
+
+    return (
+      ["twitter.com", "www.twitter.com"].includes(host) &&
+      pathname.includes("/status/")
+    );
+  },
+  // getHTML can also be async
+  getHTML(url: string) {
+    let id = /([^\/]+$)/.exec(url)?.[1];
+    return `<twitterembed id="${id}"/>`;
+  },
+};
+
 export const Post = defineDocumentType(() => ({
   name: "Post",
   contentType: "mdx",
@@ -9,6 +32,11 @@ export const Post = defineDocumentType(() => ({
   fields: {
     date: {
       type: "date",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
       required: true,
     },
     title: {
@@ -44,5 +72,6 @@ export default makeSource({
   documentTypes: [Post],
   mdx: {
     rehypePlugins: [rehypePrism],
+    remarkPlugins: [[remarkEmbedder, { transformers: [TwitterTransformer] }]],
   },
 });
