19f1487917d6229d3dcbf78f3bb431d15af29e8b
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2022-08-15 17:45:04 +0300

    tools page updated

--- pages/tools.tsx
+++ pages/tools.tsx
@@ -1,3 +1,42 @@
-export default function Tools() {
-  return <h1 className="text-sm">under construction</h1>;
+import { allTools, Tool } from "contentlayer/generated";
+import { NextSeo } from "next-seo";
+
+export async function getStaticProps() {
+  const tools = allTools.sort((a, b) => a.order - b.order);
+  return { props: { tools } };
+}
+
+export default function Tools({ tools }: { tools: Tool[] }) {
+  return (
+    <>
+      <NextSeo title="Ekipmanlar" />
+      <section>
+        <h2 className="text-3xl font-semibold">Ekipmanlar</h2>
+        <div className="gap-5 mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {tools.map((tool: Tool) => {
+            return (
+              <div
+                key={tool._id}
+                className="flex flex-col justify-center bg-zinc-100 dark:bg-zinc-800 p-3 rounded-md"
+              >
+                <figure className="aspect-square overflow-hidden rounded-md">
+                  <img
+                    src={tool.image}
+                    alt={tool.title}
+                    className="h-full w-full object-contain"
+                  />
+                </figure>
+                <header className="mt-2">
+                  <h5 className="opacity-80 text-sm mb-0.5 text-center">
+                    {tool.brand}
+                  </h5>
+                  <h3 className="font-medium text-center">{tool.title}</h3>
+                </header>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 }
